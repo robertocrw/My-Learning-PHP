ab02169a21b64df37568ca87ccfbd84acddd1f42
Roberto Pinto <robertowashington.bailao@gmail.com>
2014-12-26 15:59:19 +0000

    add a form

--- impares-index.php
+++ impares-index.php
@@ -6,15 +6,15 @@ intervalo fechado de 1 a 100.-->
 <head>
      <meta charset="UTF-8" />
      <title> Impares </title>
-     <link rel=""stylesheet" href="index.css" type="text/css"/>
+     <link rel="stylesheet" href="index.css" type="text/css"/>
 </head>
 <body>
      <div>
          <form method="post" action="impares.php" >
-         
+         Começo: <input type="number" name="comeco"/><br/>
+         Fim: <input type="number" name="fim" /><br/>
+         <input type="submit" class="botao" value="Analisar"/>
          </form>
      </div>
 </body>
 </html>
-
-<!--Continua-->
--- impares.php
+++ impares.php
@@ -6,17 +6,22 @@ intervalo fechado de 1 a 100.-->
 <head>
      <meta charset="UTF-8" />
      <title> Impares </title>
-     <link rel=""stylesheet" href="index.css" type="text/css"/>
+     <link rel="stylesheet" href="index.css" type="text/css"/>
 </head>
 <body>
      <div>
      	  <?php 
-              foreach (range(1,100) as $var1) {
+              $var1 = isset($_POST["comeco"])?$_POST["comeco"]:0;
+              $var2 = isset($_POST["fim"])?$_POST["fim"]:0;
               
+              foreach (range($var1,$var2) as $var1) {
+              //Repare que o a manipulação esta com a mesma variavel $var1
+              //do range e do inicio
               if (($var1 % 2) != 0) {
                echo  "$var1 é impar<br/>";  
               }}
      	  ?>
+        <a href="javascript:history.go(-1)" class="botao">Voltar</a>
      </div>
 </body>
 </html>
