915d998da812c0102c189721976542497e4191c7
Roberto Pinto <robertowashington.bailao@gmail.com>
2014-12-30 16:11:46 +0000

    second commit add if condtion and call the function

--- calc-index.php
+++ calc-index.php
@@ -10,7 +10,15 @@
           <form method="post" action="calc.php">
                <input type="text" name="num1"/>
                <input type="text" name="num2"/>
+               <select name="opt">
+                     <option>+</option>
+                     <option>*</option>
+                     <option>/</option>
+                     <option>-</option>
+                     <option>exp</option>
+               </select>
                <input type="submit" value="Calcular"/>
+
           </form>
      </div>  
 </body>
--- calc.php
+++ calc.php
@@ -13,17 +13,61 @@
                {
                   var $num1;
                   var $num2;
+                  
                   //var $multiplicar;
                  function somar($nu1,$nu2) 
                  {
                  	$this->num1 = $nu1;
                  	$this->num2 = $nu2;
+                  $operacao = ($_POST["opt"]);
+                  if ($operacao == "+") {
                  	echo $this->num1 + $this->num2;
+                 }
+                 } 
+                 function multiplicar($nu1,$nu2) 
+                 {
+                  $this->num1 = $nu1;
+                  $this->num2 = $nu2;
+                  $operacao = ($_POST["opt"]);
+                  if ($operacao == "*") {
+                  echo $this->num1 * $this->num2;
+                 }
+                 } 
+                 function dividir($nu1,$nu2) 
+                 {
+                  $this->num1 = $nu1;
+                  $this->num2 = $nu2;
+                  $operacao = ($_POST["opt"]);
+                  if ($operacao == "/") {
+                  echo $this->num1 / $this->num2;
+                 }
+                 } 
+                 function subtrair($nu1,$nu2) 
+                 {
+                  $this->num1 = $nu1;
+                  $this->num2 = $nu2;
+                  $operacao = ($_POST["opt"]);
+                  if ($operacao == "-") {
+                  echo $this->num1 - $this->num2;
+                  } 
+                  } 
+                 function exponencial($nu1,$nu2) 
+                 {
+                  $this->num1 = $nu1;
+                  $this->num2 = $nu2;
+                  $operacao = ($_POST["opt"]);
+                  if ($operacao == "exp") {
+                  echo pow($this->num1,  $this->num2); //Atenção para exponencial
+                 }
                  } 
 
                }
                $calcu = new calculadora;
                $calcu->somar($_POST["num1"], $_POST["num2"]);
+               $calcu->multiplicar($_POST["num1"], $_POST["num2"]);
+               $calcu->dividir($_POST["num1"], $_POST["num2"]);
+               $calcu->subtrair($_POST["num1"], $_POST["num2"]);
+               $calcu->exponencial($_POST["num1"], $_POST["num2"]);
           ?>
      </div>  
 </body>
